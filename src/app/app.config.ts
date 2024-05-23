ee2768fa4fa34b4a5414d2a34b62017f07f02809
a_ez <ez.trisoaring@gmail.com>
2024-05-23 10:03:38 +0800

    update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
@@ -11,11 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), provideFirebaseApp(() => 
-      initializeApp({"projectId":"friendlychat-76d2c","appId":"1:791594792643:web:ca9467f2298e723bcd9476","storageBucket":"friendlychat-76d2c.appspot.com","apiKey":"AIzaSyAcD0ut2oDqksqJP6xy75Ysf6I5dx1kykg","authDomain":"friendlychat-76d2c.firebaseapp.com","messagingSenderId":"791594792643"})), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore()), 
-    provideMessaging(() => getMessaging()), 
-    provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes)
   ],
 };
