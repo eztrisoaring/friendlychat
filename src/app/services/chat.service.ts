d82a80e8eb76cca63df35ab78bca06a7ee520ede
a_ez <ez.trisoaring@gmail.com>
2024-05-23 11:55:23 +0800

    fix error

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -145,7 +145,7 @@ export class ChatService {
     const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
     // Start listening to the query.
     return collectionData(recentMessagesQuery);
-  }
+  };
 
   // Saves a new message containing an image in Firebase.
   // This first saves the image in Firebase storage.
